d16ea48e9f6414c83ddcd2b347d25180b9d14d49
EsraSaydan <esracarturk.77@gmail.com>
2024-09-02 01:12:05 +0300

    done

---
+++ src/main/java/com/workintech/fswebs18challengemaven/exceptions/CardErrorResponse.java
@@ -0,0 +1,15 @@
+package com.workintech.fswebs18challengemaven.exceptions;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CardErrorResponse {
+    private String message;
+
+
+}
